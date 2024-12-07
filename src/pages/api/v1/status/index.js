9d9e45e71080276448b1dce3870f27af3ff66a64
GUTEMBERG <gtmbrglamark@gmail.com>
2024-12-07 11:11:27 -0300

    fix: removendo logs

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,11 +1,7 @@
 import database from "../../../../infra/database";
 
 export default async function status(request, response) {
-  console.log(database);
+  const data = await database.query("SELECT 1 + 1 as sum;");
 
-  const result = await database.query("SELECT 1 + 1 as sum;");
-
-  console.log(result.rows);
-
-  response.status(200).json({ chave: "são acima da média" });
+  response.status(200).json({ data });
 }
