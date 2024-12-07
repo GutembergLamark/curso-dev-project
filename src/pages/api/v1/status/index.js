00bc200a9f9554e66bac400878ab7e62324b1671
GUTEMBERG <gtmbrglamark@gmail.com>
2024-12-07 10:56:15 -0300

    feat: add variaveis de ambiente

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,3 +1,11 @@
-export default function status(request, response) {
-  response.status(200).json({ chave: "são acima da média" })
+import database from "../../../../infra/database";
+
+export default async function status(request, response) {
+  console.log(database);
+
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result.rows);
+
+  response.status(200).json({ chave: "são acima da média" });
 }
--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,5 @@
 test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
-  expect(response.status).toBe(200)
-
-})
+  expect(response.status).toBe(200);
+});
