f1e132c12369f1d21c577d9b9b4317cc5a065aa4
GUTEMBERG <gtmbrglamark@gmail.com>
2024-12-08 19:53:14 -0300

    feat: add suporte a certificados ssl autoassinados

--- src/infra/database.js
+++ src/infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'production' ? true : false
+    ssl: getSSLValues()
   });
   console.log('credenciais do Postgrs', {
     host: process.env.POSTGRES_HOST,
@@ -27,10 +27,20 @@ async function query(queryObject) {
     console.error(error);
     throw error
   } finally {
-    /* client.end(); */
+    client.end();
   }
 }
 
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === 'production' ? true : false
+}
