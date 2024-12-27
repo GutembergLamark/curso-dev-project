6262c1a01fc82102e77eaf8ea228122bfe78fb0b
GUTEMBERG <gtmbrglamark@gmail.com>
2024-12-27 15:56:33 -0300

    fix: prettier style linting

--- src/infra/scripts/wait-for-postgres.js
+++ src/infra/scripts/wait-for-postgres.js
@@ -1,10 +1,12 @@
-const { exec } = require('node:child_process')
+const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec('docker exec postgres_database pg_isready --host localhost', handleReturn);
+  exec(
+    "docker exec postgres_database pg_isready --host localhost",
+    handleReturn,
+  );
 
   function handleReturn(error, stdout, stderr) {
-
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
       checkPostgres();
--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,17 +1,26 @@
 import database from "infra/database";
 
 export default async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
 
-  const databaseName = process.env.POSTGRES_DB
-  const databaseOpenedConnections = await database.query({ text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", values: [databaseName] })
-  const databaseOpenedConnectionsValue = databaseOpenedConnections.rows[0].count
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
 
-  const databaseMaxConnections = await database.query({ text: "SHOW max_connections;" })
-  const databaseMaxConnectionsValue = databaseMaxConnections.rows[0].max_connections
+  const databaseMaxConnections = await database.query({
+    text: "SHOW max_connections;",
+  });
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
 
-  const databaseVersion = await database.query({ text: "SHOW server_version;" })
-  const databaseVersionValue = databaseVersion.rows[0].server_version
+  const databaseVersion = await database.query({
+    text: "SHOW server_version;",
+  });
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
 
   return response.status(200).json({
     updated_at: updatedAt,
@@ -19,8 +28,8 @@ export default async function status(request, response) {
       database: {
         opened_connections: databaseOpenedConnectionsValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        version: databaseVersionValue
-      }
-    }
+        version: databaseVersionValue,
+      },
+    },
   });
 }
