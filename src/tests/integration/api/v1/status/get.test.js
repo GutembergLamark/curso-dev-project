3fdc0fbce4f560312722c8c3fc15442efabe56e2
GUTEMBERG <gtmbrglamark@gmail.com>
2025-01-03 10:03:11 -0300

    test: rename all descriptions to use new pattern

--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,22 @@ beforeAll(async () => {
   await orquestrator.waitForAllServices();
 });
 
-test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", function () {
+  describe("Anonymous user", function () {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+      const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toBe("16.0");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
+
